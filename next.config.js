1c30b19998a8256003d59d58cf25697b0a261b4d
Bohdan <bogdank208@gmail.com>
2023-06-27 19:22:26 +0300

    asdsad

--- next.config.js
+++ next.config.js
@@ -2,23 +2,12 @@ const { PHASE_DEVELOPMENT_SERVER, PHASE_EXPORT, PHASE_PRODUCTION_BUILD, PHASE_PR
 
 module.export = (phase) => {
 
-  if(phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: process.env.MONGODB_USERNAME,
-        mongodb_password: process.env.MONGODB_PASSWORD,
-        mongodb_cluster: process.env.MONGODB_CLUSTER,
-        mongodb_db: process.env.MONGODB_DB,
-        someOtherVarName: 'someOtherVarValue'
-      }
-    }
-  }
   return {
     env: {
-      mongodb_username: 'bogdank208',
-      mongodb_password: 'bogdank208',
-      mongodb_cluster: 'cluster0.dgcghix.mongodb.net',
-      mongodb_db: 'nextjsCourse'
+      mongodb_username: process.env.MONGODB_USERNAME,
+      mongodb_password: process.env.MONGODB_PASSWORD,
+      mongodb_cluster: process.env.MONGODB_CLUSTER,
+      mongodb_db: process.env.MONGODB_DB
     }
   }
   
